fee274a0ef13e59f170a3b3bfdfbe80cec36d17c
1181259130 <1181259130zwm@gmail.com>
2025-03-09 18:56:39 +0800

    Update and rename mkqqyy.js to zzmusicplay.js

--- zzmusicplay.js
+++ zzmusicplay.js
@@ -1,11 +1,3 @@
-/* 
- * 
-[rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/zxs-ai/Xnet/refs/heads/main/zzmusicplay.js
-
-[mitm]
-hostname = api.dragonlongzhu.cn
-*/
 //mikoto - 修改点歌卡片的封面
 //使用方法：圈x 添加主机名  api.dragonlongzhu.cn
 //修改本配置的文件名为mkqqyy.js
@@ -13,19 +5,26 @@ hostname = api.dragonlongzhu.cn
 //修改本配置显示文本和封面链接
 //然后点歌
 
+
 let body = $response.body;
 if (body) {
   try {
     let obj = JSON.parse(body);
     if (obj && obj.data) {
-      obj.data.song_name = ${obj.data.song_name || ""}-${obj.data.song_singer || ""};
+   
+      let originalName = obj.data.song_name || "";
+      let originalSinger = obj.data.song_singer || "";
+    
+      obj.data.song_name = originalName + "-" + originalSinger;
+      // 将歌手改为固定文本“点击播放—>”
       obj.data.song_singer = "z先生定制电台>>>";
+      // 修改封面为指定链接
       obj.data.cover = "http://q4.qlogo.cn/headimg_dl?dst_uin=719883788&spec=640";
     }
-    $done({ body: JSON.stringify(obj) });
+    $done({body: JSON.stringify(obj)});
   } catch (e) {
     console.log("解析失败:", e);
-    $done({ body });
+    $done({body});
   }
 } else {
   $done({});
