e521166b3c840961c37b8c3bc5071621ae7e9007
1181259130 <1181259130zwm@gmail.com>
2025-03-09 18:52:20 +0800

    Update mkqqyy.js

--- mkqqyy.js
+++ mkqqyy.js
@@ -1,3 +1,11 @@
+/* 
+ * 
+[rewrite_local]
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://github.com/1181259130/kongcheng/edit/main/mkqqyy.js
+
+[mitm]
+hostname = api.dragonlongzhu.cn
+*/
 //mikoto - 修改点歌卡片的封面
 //使用方法：圈x 添加主机名  api.dragonlongzhu.cn
 //修改本配置的文件名为mkqqyy.js
@@ -5,31 +13,19 @@
 //修改本配置显示文本和封面链接
 //然后点歌
 
-[rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://github.com/1181259130/kongcheng/blob/main/mkqqyy.js
-
-[mitm]
-hostname = api.dragonlongzhu.cn
-
 let body = $response.body;
 if (body) {
   try {
     let obj = JSON.parse(body);
     if (obj && obj.data) {
-   
-      let originalName = obj.data.song_name || "";
-      let originalSinger = obj.data.song_singer || "";
-    
-      obj.data.song_name = originalName + "-" + originalSinger;
-      // 将歌手改为固定文本“点击播放—>”
+      obj.data.song_name = ${obj.data.song_name || ""}-${obj.data.song_singer || ""};
       obj.data.song_singer = "W.定制电台>>>";
-      // 修改封面为指定链接
       obj.data.cover = "http://q4.qlogo.cn/headimg_dl?dst_uin=719883788&spec=640";
     }
-    $done({body: JSON.stringify(obj)});
+    $done({ body: JSON.stringify(obj) });
   } catch (e) {
     console.log("解析失败:", e);
-    $done({body});
+    $done({ body });
   }
 } else {
   $done({});
