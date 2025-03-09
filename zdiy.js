174ec778cff1c9c16fad4d6fdc50b370575fa747
1181259130 <1181259130zwm@gmail.com>
2025-03-09 19:00:55 +0800

    Update zdiy.js

--- zdiy.js
+++ zdiy.js
@@ -1,5 +1,5 @@
 [rewrite_local]
-^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/1181259130/kongcheng/refs/heads/main/zzmusicplay.js
+^https:\/\/api\.dragonlongzhu\.cn\/.* url script-response-body https://raw.githubusercontent.com/1181259130/blob/main/zzmusicplay.js
 
 [mitm]
 hostname = api.dragonlongzhu.cn
